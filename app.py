9aa6cd8875ff683a599fece47eb6227378430443
harshalgalgale <harshal@kisanhub.com>
2020-04-09 20:49:49 +0100

    added add function and user profile function

--- app.py
+++ app.py
@@ -4,3 +4,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return {'data': 'Hello, World! Welcome to Flask'}
+
+
+@app.route('/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User {}'.format(username)
+
+
+@app.route('/add/<numbers>')
+def add(numbers):
+    # show the user profile for that user
+    numbers = numbers.split(',')
+    return 'Add {}'.format(int(numbers[0])+int(numbers[1]))
